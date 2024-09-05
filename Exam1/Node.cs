afca3df489613b69649e4ffb5084ee9e23fdf69c
rafi053 <rafi053313@gmail.com>
2024-09-05 12:38:46 +0300

    Functions GetTreeString() added

--- Exam1/Node.cs
+++ Exam1/Node.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 namespace Exam1
 {
     public class Node
-    { 
+    {
         public int MinSeverity { get; set; }
         public int MaxSeverity { get; set; }
         public List<string> Defenses { get; set; }
-        public Node LeftSun {  get; set; }
+        public Node LeftSun { get; set; }
         public Node RightSun { get; set; }
 
         public Node(int MinSeverity, int MaxSeverity, List<string> Defenses)
@@ -19,10 +19,19 @@ namespace Exam1
             this.MinSeverity = MinSeverity;
             this.MaxSeverity = MaxSeverity;
             this.Defenses = Defenses;
-            LeftSun =null;
+            LeftSun = null;
             RightSun = null;
         }
+        public string GetNodeStringWithoutChild()
+        {
+            string result = $"Range: [{MinSeverity},{MaxSeverity}]' Defenses:";
+            if (Defenses == null || Defenses.Count == 0)
+                result += " empty";
+            foreach (string s in Defenses)
+                result += ", " + s;
+            result += ";";
+            return result;
+        }
+
     }
-
-
 }
---
+++ Exam1/Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exam1
+{
+    public class Tree
+    {
+        public Node Root {  get; set; }
+        public Tree() { }
+        public Tree(Node Root)
+        {
+            this.Root = Root;
+        }
+        //public void Insert(Node node) {}
+
+        public string GetTreeString()
+        {
+            if (Root == null)
+                return "Empty tree";
+            return Root.GetNodeStringWithoutChild() + GetTreeString(Root.LeftSun) + GetTreeString(Root.RightSun);
+        }
+        private string GetTreeString(Node node)
+        {
+            if (node == null)
+                return "";
+            return "\n" + node.GetNodeStringWithoutChild() + GetTreeString(node.LeftSun) + GetTreeString(node.RightSun);
+        }
+    }
+}
