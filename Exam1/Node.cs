96c21e1eb0098e1e6e7f9968b98af190c82ba186
rafi053 <rafi053313@gmail.com>
2024-09-05 12:14:17 +0300

    Add project files.

---
+++ Exam1/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1
+{
+    public class Node
+    { 
+        public int MinSeverity { get; set; }
+        public int MaxSeverity { get; set; }
+        public List<string> Defenses { get; set; }
+        public Node LeftSun {  get; set; }
+        public Node RightSun { get; set; }
+
+        public Node(int MinSeverity, int MaxSeverity, List<string> Defenses)
+        {
+            this.MinSeverity = MinSeverity;
+            this.MaxSeverity = MaxSeverity;
+            this.Defenses = Defenses;
+            LeftSun =null;
+            RightSun = null;
+        }
+    }
+
+
+}
