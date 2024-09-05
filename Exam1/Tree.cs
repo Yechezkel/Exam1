96e8ca3daf4a06343bb34c3d170303bacade9630
rafi053 <rafi053313@gmail.com>
2024-09-05 13:04:32 +0300

    Function Insert(Node node) added

--- Exam1/Tree.cs
+++ Exam1/Tree.cs
@@ -11,23 +11,60 @@ namespace Exam1
     {
         public Node Root {  get; set; }
         public Tree() { }
-        public Tree(Node Root)
+        //public Tree(Node Root)
+        //{
+        //    this.Root = Root;
+        //}
+        public void Insert(Node node)
         {
-            this.Root = Root;
+            if (Root == null)
+            {
+                Root = node;
+                return;
+            }
+            Node tmp = Root;
+            while (true)
+            {
+                if (node.MaxSeverity < tmp.MaxSeverity)//assuming if (node.MaxSeverity < tmp.MaxSeverity) => (node.MaxSeverity < tmp.MinSeverity)
+                {
+                    if (tmp.LeftSun == null)
+                    {
+                        tmp.LeftSun = node;
+                        return;
+                    }
+                    else
+                        tmp = tmp.LeftSun;
+                }
+                else
+                {
+                    if (tmp.RightSun == null)
+                    {
+                        tmp.RightSun = node;
+                        return;
+                    }
+                    else
+                        tmp = tmp.RightSun;
+                }
+            }
         }
-        //public void Insert(Node node) {}
+
+
+
 
         public string GetTreeString()
         {
             if (Root == null)
                 return "Empty tree";
-            return Root.GetNodeStringWithoutChild() + GetTreeString(Root.LeftSun) + GetTreeString(Root.RightSun);
+            return "Root = "+Root.GetNodeStringWithoutChild() + GetTreeString(Root.LeftSun, "Left", 1) + GetTreeString(Root.RightSun,"Right",1);
         }
-        private string GetTreeString(Node node)
+        private string GetTreeString(Node node ,string side,int tabs)
         {
             if (node == null)
                 return "";
-            return "\n" + node.GetNodeStringWithoutChild() + GetTreeString(node.LeftSun) + GetTreeString(node.RightSun);
+            string str = "\n";
+            for (int i = 0; i < tabs; i++)
+                str += "-";
+            return str +side +" = "+ node.GetNodeStringWithoutChild() + GetTreeString(node.LeftSun,"Left", tabs+1) + GetTreeString(node.RightSun,"Right", tabs + 1);
         }
     }
 }
